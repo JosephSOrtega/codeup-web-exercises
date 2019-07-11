87e4b86383d49eb8189df4dfa73b9761fe194e74
Justin Saint Joseph Ortega <justinsortega@justins-mbp.attlocal.net>
2019-07-11 18:08:50 -0500

    eod thurs

--- Pet Proj/Class Notes.js
+++ Pet Proj/Class Notes.js
@@ -47,13 +47,13 @@
 //
 //                                      jslint.com to check syntax
 //
-//
+//          Asynchronous JavaScript and XML)
 //
 //          A Higher Order Function- A higher order function is a function that takes a function as an argument, or returns a function . Higher order function is in contrast to first order functions, which don't take a function as an argument or return a function as output.Mar 3, 2017
 //
+//          Stays inside the local scope
 //
-//
-//
+//          Request verification
 //
 
 
---
+++ js/keys.js
@@ -0,0 +1,3 @@
+var mapBoxAccessKey = 'pk.eyJ1IjoianVzdGluc29ydGVnYSIsImEiOiJjanhhdXhrcjMwMzk2M3ByOHp2Zjd0dGpuIn0.p8LhRGew39ks7ejs7AQw8Q';
+var darkSkyKey ="916b76026826b346fc1ed2a625005318";
+
