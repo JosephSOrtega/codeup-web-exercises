7df55ab7d5adee326ce67a0d7c6d95b346e50ede
JustinSOrtega <justinsortega@gmail.com>
2019-07-16 15:57:30 -0500

    eod Tuesday. Need to polish fade navbar

--- Pet Proj/RDM Site/homepage.js
+++ Pet Proj/RDM Site/homepage.js
@@ -2,14 +2,41 @@
 
 
 (function () {
-        $(window).scroll(function () {
-            $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 800);
-            $(".navbar").css("opacity", 1 - $(window).scrollTop() / 800);
-            if (($('.navbar').css("opacity")) < .2) {
-                $('.navbar').css("opacity", ".2")
+    let opac;
+
+    function fadeInEffect() {
+        $('.navbar').animate({
+            opacity: 1,
+        }, 200, "linear", {
+            step: function () {
             }
+        })
+    }
+
+    function fadeOutEffect() {
+        $('.navbar').animate({
+            opacity: opac,
+        }, 100, "linear", {
+            step: function () {
+            }
+        })
+    }
+
+    $(window).scroll(function () {
+        $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 800);
+        $(".navbar").css("opacity", 1 - $(window).scrollTop() / 800);
+        if (($('.navbar').css("opacity")) < .1) {
+            $('.navbar').css("opacity", ".1")
+        }
+        $('.navbar').hover(function () {
+            opac = $('.navbar').css("opacity");
+
+            fadeInEffect()
+        }, function () {
+            fadeOutEffect()
 
         });
+    });
 
 
 })();
