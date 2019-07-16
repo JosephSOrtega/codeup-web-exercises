594a8ab8c861ebdd2061fec1f868c6676183a29d
JustinSOrtega <justinsortega@gmail.com>
2019-07-16 17:21:30 -0500

    Fixed the fade and hover issues. Now time for some dinner.

--- Pet Proj/RDM Site/homepage.js
+++ Pet Proj/RDM Site/homepage.js
@@ -4,6 +4,8 @@
 (function () {
     let opac;
 
+
+    // Keeps breaking
     function fadeInEffect() {
         $('.navbar').animate({
             opacity: 1,
@@ -25,14 +27,24 @@
     $(window).scroll(function () {
         $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 800);
         $(".navbar").css("opacity", 1 - $(window).scrollTop() / 800);
-        if (($('.navbar').css("opacity")) < .1) {
-            $('.navbar').css("opacity", ".1")
+        if (($('.navbar').css("opacity")) < .2) {
+            $('.navbar').css("opacity", ".2")
         }
+    });
+    $(document).ready(function () {
+
+
         $('.navbar').hover(function () {
             opac = $('.navbar').css("opacity");
+            //remove for func version to work
+            // $('.navbar').css("opacity", "1");
 
             fadeInEffect()
         }, function () {
+
+            //remove for func version to work
+            // $('.navbar').css("opacity", opac);
+
             fadeOutEffect()
 
         });
